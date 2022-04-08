5e90e70bacc0b6ee294af508e780793d69d90952
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2022-04-08 19:47:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 package com.company;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String result = "";;
         System.out.print("Введите предложение: ");
         String word = in.nextLine();
+        minWordLength(word);
+    }
+    public static void minWordLength(String word){
+        String result;
         int min = word.length();
         String[] words = word.split(" ");
         for(String t : words){
@@ -15,7 +19,7 @@ public class Main {
                 min = c;
             }
         }
-        System.out.printf("Длина самого короткого слова равна %d символа(ов)\nСлово(а): \n", min);
+        System.out.printf("Длина минимально слова равна %d символа(ов)\nСлово(а): \n", min);
         for(String t : words){
             if(t.length() == min){
                 result = t;
